36c8646afee88564d6e00cd5228672561d8615b1
Honza Hruby <jan.hruby@inloopx.com>
2018-08-06 13:52:01 +0200

    Upgrade typescript to version 3

--- lib/ChartWrapper/ChartWrapper.d.ts
+++ lib/ChartWrapper/ChartWrapper.d.ts
@@ -1,4 +1,4 @@
 import * as React from 'react';
 import { CategoricalChartWrapper } from 'recharts';
 import { IResourceChartProps } from './ResourceChartProps';
-export declare const chartWrapper: (ChartComponent: React.ComponentType<CategoricalChartWrapper<"centric" | "horizontal" | "vertical">>) => (props: IResourceChartProps) => JSX.Element;
+export declare const chartWrapper: (ChartComponent: React.ComponentType<CategoricalChartWrapper<"horizontal" | "vertical" | "centric">>) => (props: IResourceChartProps) => JSX.Element;
