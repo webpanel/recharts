f653b009314cc1c231969dc785656425fc0125b8
Honza Hruby <jan.hruby@inloopx.com>
2018-08-07 13:30:31 +0200

    Add scatter chart

---
+++ src/ScatterChart/ResourceScatterChart.tsx
@@ -0,0 +1,4 @@
+import { ScatterChart } from 'recharts';
+import { chartWrapper } from '../ChartWrapper';
+
+export const ResourceScatterChart = chartWrapper(ScatterChart);
---
+++ src/ScatterChart/ResourceScatterChartProps.tsx
@@ -0,0 +1,7 @@
+import { ScatterChartProps } from 'recharts';
+import { ResourceCollection } from 'webpanel-data';
+
+export interface IResourceScatterChartProps extends ScatterChartProps {
+  resourceCollection: ResourceCollection;
+  render?(data: any): React.ReactNode | React.ReactNodeArray;
+}
